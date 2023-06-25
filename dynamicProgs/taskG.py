2954a3bb90494eb34577f13707ab198e43ff0770
AlexeyVaki <puchinin.alyosha@yandex.ru>
2023-06-25 22:58:29 +0300

    Well, i don't like this version, but it works!

--- dynamicProgs/taskG.py
+++ dynamicProgs/taskG.py
@@ -16,14 +16,23 @@ class taskG:
             None
         elif n == 0:
             self.ex.append([MAX_V, MAX_V, MAX_V])
-        elif n == 1:
-            a = self.m1_or_m2(n, 1)
+        if n >= 1:
+            a = self.m1_or_m2(1, 1)
             b = MAX_V
             self.ex.append([a, b, min(a, b)])
-        elif n == 2:
+        if n >= 2:
             a = MAX_V
-            b = self.m1_or_m2(n, 2)
+            b = self.m1_or_m2(2, 2)
             self.ex.append([a, b, min(a, b)])
+        if n >= 3:
+            a = self.m1_or_m2(3, 1) + self.ex[3-1-2][2]
+            b = MAX_V
+            self.ex.append([a, b, min(a, b)])
+        for i in range (4, n+1):
+            a = self.m1_or_m2(n, 1) + self.ex[n-1-2][2]
+            b = self.m1_or_m2(n, 2) + self.ex[n-1-3][2]
+            self.ex.append([a, b, min(a, b)])
+            
         
         
         return self.ex[n - 1][2]
@@ -34,6 +43,6 @@ class taskG:
         
 
 # s = [0, 2, 5, 12]
-s = [0, 2, 5]
+s = [0, 2, 5, 12, 15, 23]
 a = taskG(s)
 print(a.get())
