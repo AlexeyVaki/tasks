60760fdd132039d129cac7895ae0c9436d9515f9
AlexeyVaki <puchinin.alyosha@yandex.ru>
2023-07-19 10:57:44 +0300

    j_now

--- dynamicProgs/TaskJ.py
+++ dynamicProgs/TaskJ.py
@@ -1,22 +1,24 @@
 class TaskJ:
     def __init__(self) -> None:
-        self.d = {0: 1}
+        self.d = {}
 
     def get(self, s):
+        self.d[0] = [s[0]]
         for i in range(1, len(s)):
-            m = 1
-            for j in self.d.keys():
+            m = []
+            for j in range(len(self.d)):
                 if s[j] < s[i]:
-                    if self.d[j] < m:
-                        m = self.d[j]
-                    m += 1
+                    if len(self.d[j]) > len(m):
+                        m = self.d[j].copy()
+                    
+            m += [s[i]]
             self.d[i] = m
-        return max(self.d.values())
+        return max(self.d, key=self.p)
+
+    def p(self, k):
+        return len(self.d[k])
 
 j = TaskJ()
-j1 = j.get([10, 11, 12, 13, 11, 12, 13, 14, 15])
+j1 = j.get([3, 29, 5, 5, 28, 7])
 print(j.d)
-print(j1)
-
-
-
+print(j.d[j1])
